a8453a897499c22b44ae2d30c1b417853bd23c37
mcast2185 <76968385+mcast2185@users.noreply.github.com>
2022-10-26 23:32:25 -0600

    update

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -24,18 +24,19 @@ import {
   IonListHeader
 } from '@ionic/react';
 
-
 import {
   LatLit,
   DirectionsResult,
   MapOptions
 } from './Type';
 
+import googleapis, { GoogleApis, google,  } from "googleapis";
 
 const apiKey = String(process.env.REACT_APP_GOOGLE_API_KEY);
 
 
 const Order = () => {
+  // let Gapi: gapi.client.
   const [lat, setLat] = useState<number>();
   const [lng, setLng] = useState<number>();
   const myLocation = async () => {
@@ -47,24 +48,30 @@ const Order = () => {
   myLocation().then(res => {
     return res
   });
-  
 
   const urlQuery = () => {
-    const url = fetch('https://www.google.com/search?q=barber%20shop%20near%20me&oq=barber&aqs=chrome.1.69i57j35i39l2j0i433i457i512j0i402l2j46i175i199i512j0i433i512j46i175i199i512l2.2931j0j7&sourceid=chrome&ie=UTF-8&rflfq=1&tbm=lcl&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e3!2m4!1e2!5m2!2m1!2e9!3sIAE,lf:1,lf_ui:14&ved=2ahUKEwiN2bSL0v36AhVuIDQIHTipCsAQwywoAXoECBQQBg&rlst=f#rlfi=hd:;si:;mv:[[39.871139199999995,-104.95535910000001],[39.769417,-105.0597061]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!1m5!1u2!3m2!2m1!2e9!4e2!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:14', 
-    {  
-      method: 'GET',    
-      mode: 'no-cors'    
-    }
-    )
-    // url.then((response) => response)
-    // .then((data) => console.log(data));
-    
+    gapi.client.setApiKey(apiKey);
+    return gapi.client.load("https://www.google.com/search?q=barber+shop+near+me&oq=barber+shop+near+me&aqs=chrome..69i57j69i59j0i512l8.4644j0j7&sourceid=chrome&ie=UTF-8", "v1")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+    // const url = fetch('https://www.google.com/search?q=barber%20shop%20near%20me&oq=barber&aqs=chrome.1.69i57j35i39l2j0i433i457i512j0i402l2j46i175i199i512j0i433i512j46i175i199i512l2.2931j0j7&sourceid=chrome&ie=UTF-8&rflfq=1&tbm=lcl&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e3!2m4!1e2!5m2!2m1!2e9!3sIAE,lf:1,lf_ui:14&ved=2ahUKEwiN2bSL0v36AhVuIDQIHTipCsAQwywoAXoECBQQBg&rlst=f#rlfi=hd:;si:;mv:[[39.871139199999995,-104.95535910000001],[39.769417,-105.0597061]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!1m5!1u2!3m2!2m1!2e9!4e2!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:14', 
+    // {  
+    //   method: 'GET',    
+    //   mode: 'no-cors'    
+    // }
+    // )
   }
-
+  
+  // const test = () => {
+  //   gapi.client.search.cse.list({})
+  // }
 
   useEffect(() => {
-    return urlQuery
+    urlQuery();
+    
   })
+
+
   return (
     <IonList>
     <IonListHeader>
